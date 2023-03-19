023c7bfbb7307f83662a0eb941c25c67ee7c7d37
Vishalpateldodla <117738669+Vishalpateldodla@users.noreply.github.com>
2023-03-19 18:56:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function updateOutput() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  }).then((message) => {
+    document.getElementById("output").innerHTML = message;
+  });
+}
+
+updateOutput();
